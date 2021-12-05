968cf494cb28fd8d07a4b66a4d728adadfd9544c
Joe Giuffrida <conquercat24@gmail.com>
2021-12-05 15:11:19 -0800

    updating mobile version of stats preview card

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,45 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <>
-      <div>THIS APP SUCKS</div>
+    <div className={styles.wrapper}>
+      <Head>
+        <title>Stats Preview Card</title>
+        <link rel="icon" href="favicon-32x32.jpg" type="image/x-icon" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Lexend+Deca&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className={styles.card}>
+        <header className={styles.cardImage}>
+          <Image src="/image-header-mobile.jpg" width="654" height="480" />
+          <div className={styles.overlay} />
+        </header>
 
-      <h1>wtf is going on</h1>
-    </>
+        <section className={styles.cardContent}>
+          <h2>
+            Get <span>insights</span> that help your business grow.
+          </h2>
+          <p>
+            Discover the benefits of data analytics and make better decisions
+            regarding revenue, customer experience, and overall efficiency.
+          </p>
+          <ul>
+            <li>
+              <h3>10k+</h3>
+              <p>COMPANIES</p>
+            </li>
+            <li>
+              <h3>314</h3>
+              <p>TEMPLATES</p>
+            </li>
+            <li>
+              <h3>12M+</h3>
+              <p>QUERIES</p>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </div>
   );
 }
